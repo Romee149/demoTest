51d4ddf543428dddbab4d1b37f013dbbf757f6ff
romee <romeeafroz@yahoo.com>
2020-04-02 20:14:30 -0500

    New file add git2

---
+++ src/testscript/gittest2.java
@@ -0,0 +1,14 @@
+package testscript;
+
+
+	import org.testng.annotations.Test;
+
+	public class gittest2 {
+		@Test
+	public static void test() {
+		System.out.println("this is my git test class");
+		System.out.println("test");
+	}
+	}
+
+
