e5642eeb654714658f05b86124a6cfca56fe60cc
romee <romeeafroz@yahoo.com>
2020-04-02 19:54:17 -0500

    Update test package

---
+++ src/testscript/gittest.java
@@ -0,0 +1,11 @@
+package testscript;
+
+import org.testng.annotations.Test;
+
+public class gittest {
+	@Test
+public static void test() {
+	System.out.println("this is my git test class");
+	System.out.println("test");
+}
+}
